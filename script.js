be40bd0466c8aac49cbb50f824ea4d0bb9b8b8ba
Ngoakoana <ngoakoana.mphago460@gmail.com>
2024-07-05 12:36:07 +0200

    added functions

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-let data = JSON.parse(localStorage.getItem('data')) || [];
-    // let data = []
-    
-    loadTasksFromLocStorage(); // load tasks from local storage 
-    readAll(); 
-    
+
+    let data = []
+
+
+    function addExpense(expenseName, expenseAmount) {
+        const expense = {
+          name: expenseName,
+          amount: parseFloat(expenseAmount)
+        };
+    }
+        expense.push(expense);
+        document.getElementById('expense-name').value = '';
+        document.getElementById('expense-amount').value = '';
+
+        updateExpenseList();
+       updateTotalAmount();
+
+
+      expense-table.forEach((expense, index) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${expense.name}</td>
+      <td>${expense.amount}</td>
+      <td><button onclick="deleteExpense(${index})">Delete</button></td>
+    `;
+    expenseList.appendChild(row);
+  });
+
+
+
+
+
+
+
+
+
+
+
